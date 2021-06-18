68dc079ebe648956ea485c63d2a5d6ea3ec6ca09
chhaya <chhptl8@gmail.com>
2021-06-18 11:09:38 +0530

    component on button

--- src/views/Sample.vue
+++ src/views/Sample.vue
@@ -1,49 +1,52 @@
 <template>
-
-<div class="container">
+  <div class="container">
     <div class="sample-page">
-        <div class="row nav-bar"><navigation-bar></navigation-bar></div>
-        <div class="row banner">
-            <banner-img>
-            </banner-img>
-
+      <div class="row nav-bar"><navigation-bar></navigation-bar></div>
+      <div class="row banner">
+        <banner-img> </banner-img>
+      </div>
+      <div class="row card-tile">
+        <div class="col-md-3">
+          <button
+            class="mx-2 mt-2"
+            style="text-transform: capitalize"
+            outlined
+            rounded
+            color="#ffab01"
+            dark
+            small
+            @click="visible = !visible"
+          >
+            Toggle Component
+          </button>
+          <my-card v-if="visible"> </my-card>
         </div>
-        <div class="row card-tile">
-            <div class="col-md-3"><my-card> </my-card></div>
-            <div class="col-md-3"> <my-card> </my-card></div>
-            <div class="col-md-3"><my-card> </my-card></div>
-            <div class="col-md-3"><my-card> </my-card></div>            
+        <div class="col-md-3"><my-card> </my-card></div>
+        <div class="col-md-3"><my-card> </my-card></div>
+        <div class="col-md-3"><my-card> </my-card></div>
+      </div>
+      <div class="row table-form">
+        <div class="col-md-3"><list-name></list-name></div>
+        <div class="col-md-9">
+          <login-form> </login-form>
         </div>
-        <div class="row table-form">
-            <div class="col-md-3"><list-name></list-name> </div>
-            <div class="col-md-9">
-                <login-form>
-                </login-form>
-            </div>
-        </div>
-
+      </div>
     </div>
-
-</div>
-    
+  </div>
 </template>
 
 <script>
-
 export default {
-  
-   
-   
-
+  data() {
+    return {
+      visible: false,
+    };
+  },
 };
-
-    
-   
-
 </script>
 
 <style>
 .card-tile {
-    padding-bottom: 25px;
+  padding-bottom: 25px;
 }
 </style>
